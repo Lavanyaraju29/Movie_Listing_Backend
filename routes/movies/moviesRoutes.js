2d3c0725a3b250bad8d7d446ec1d0c21dc7fd40e
Lavanyaraju29 <slavanya2901@gmail.com>
2024-08-25 14:07:23 +0530

    added

--- routes/movies/moviesRoutes.js
+++ routes/movies/moviesRoutes.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../../db/schemas/movieSchema");
 
-router.get("/", (req,res) => {
-    res.json({
-        message:"Hello World!",
+router.get("/", async(req,res) => {
+    const movies = await Movie.find();
+    res.json(movies);
+    }
+);
+
+router.post("/",async(req,res)=>
+    {
+        try{
+        console.log(req.body);
+        const moviesData = req.body;
+        const newMovie = new Movie(moviesData);
+        await newMovie.save();
+        res.json({
+            message: "Movie added successfully",
+        });
+    } catch(err){
+        console.log(err);
+        res.status(500).json({
+            message: "server is not stopped",
+        });
+    }
     });
-
-});
-
+    
 module.exports = router;
